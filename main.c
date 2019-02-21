45bfe3ab4076533b8a04f58bcbe84ab4e0f4ff7b
SORDET Stephane <Stephane.SORDET@cpnv.ch>
2019-02-21 11:04:23 +0100

    fonction factoriel

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int factorial(int f) {
+    if (f == 1) {
+        return 1;
+    }
+    return (f * factorial((f-1)));
+}
+
+int main()
+{
+    int fact=4;
+    printf("%d! = %d \n", fact, factorial(fact));
+    return 0;
+}
